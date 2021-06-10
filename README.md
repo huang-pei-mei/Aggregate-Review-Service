e8d18acfb7739753ee142e9dae2868cfcf17d9ac
Aschale <aschaled@gmail.com>
2021-06-09 23:09:40 -0500

    Update to include the book title

--- README.md
+++ README.md
@@ -1,32 +1,34 @@
-# Audible Aggergate Review
-
-This is an mock-up audible item page built during the Hack Reactor immersive's Front End Capstone project. This project is designed to maintain all original functionality and mimic the style of audible item page. This component of the project - Aggergate Review Service - displayes the aggeregate review information for a given book.
-
-## Related Projects
-
-  - https://github.com/huang-pei-mei/title-service
-  - https://github.com/huang-pei-mei/reviews-service
-  - https://github.com/huang-pei-mei/price-service
-  - https://github.com/huang-pei-mei/FEC-Publishers-Summary
+Project Aggergate Review Service
 
 ## Table of Contents
+- [Overview: Audible Aggergate Review](#overview-audible-aggergate-review)
+  - [Related Services](#related-services)
+  - [My contributions](#my-contributions)
+  - [Requirements](#requirements)
+  - [Development](#development)
+    - [Usage](#usage)
+  - [Technologies Used](#technologies-used)
+  - [Copyright and License](#copyright-and-license)
 
-1. [Usage](#Usage)
-1. [Requirements](#requirements)
-1. [Development](#development)
 
-## Usage
-Install dependencies (See section below)
-Run 'npm run webpack' to transpile .jsx files.
-Run 'npm start' and navigate to localhost:1220 in your browser.
+# Overview: Audible Aggergate Review
+This is an mock-up audible item page built during the Hack Reactor immersive's Front End Capstone project. This project is designed to maintain all original functionality and mimic the style of audible item page. This component of the project - Aggergate Review Service - displayes the aggeregate review information for a given book.
+
+## Related Services
+
+ - https://github.com/huang-pei-mei/title-service
+ - https://github.com/huang-pei-mei/reviews-service
+ - https://github.com/huang-pei-mei/price-service
+ - https://github.com/huang-pei-mei/FEC-Publishers-Summary
+
+## My contributions
+Designed and built the Aggregate Review Service.
+Coordinated and deployed entire team’s modules on proxy server in AWS EC2 instance.
+Utilized agile workflow practices on a 4 -person remote team.
 
 ## Requirements
-
-An `nvmrc` file is included if using [nvm](https://github.com/creationix/nvm).
-Node 6.13.0
-npm v3.5.2
-MongoDB v3.6.3
-
+An `nvmrc` file is included if using [nvm](https://github.com/creationix/nvm)
+Node 6.13.0.
 
 ## Development
 The database folder includes two helper functions which generates random data and seed data into the database. The first helper function which generates random data and save the generated data into the databaseis located in database/seed.js file. The mongo db setup file which specifies the database schema is located in database/index.js file
@@ -37,13 +39,66 @@ The server with two endpoints are located in server/index.js. The first endpoint
 
 The second endpont`/api/reviews/:bookIds` will accept an array of book ids and returns an array of objects with the the aggregate review information book ids included in the request parameters as per the schema specified in the database/index.js file.
 
-### Installing Dependencies
+### Usage
+Clone the repository from github into your local machine to make edits.
+Code is available to view on github at https://github.com/huang-pei-mei/FEC-Agg.Review.
+Once you clone the repository, from within the root directory:
 
-Run the following commands from within the root directory to run the services in your local machione once you pulle down the repository:
-
-```sh
-npm install -g webpack
 npm install
+This will install the main react front end and server side dependencies.
+
+
+Make sure to install Mongo DB and then from the root directory:
+
+npm run database/seed
+Seed the database
+
+npm run react-dev
+Builds the app for production to the public folder. It correctly bundles React in production mode and optimizes the build for the best performance.
+
+Make sure nodemon is installed on your system and then from the root directory:
+
+npm run start
+Runs the app in the development mode. Open http://localhost:2880 to view it in the browser.
+
+## Technologies Used
+  JavaScript
+  React
+  Node/Express
+  Mongo Db
+  Webpack
+  Babel
+  Jest
+  Enzyme
+  SuperTest
+  AWS (EC2, S3)
+
+
+## Copyright and License
+The MIT License (MIT) http://www.opensource.org/licenses/mit-license.php
+
+
+
+
+
+
+
+
+<!-- ### Installing
+Clone the repository from github onto your local machine to make edits. However page will not run locally with data because the service routes are directed to an AWS EC2 instance
+
+Code is available to view on github at https://github.com/huang-pei-mei/FEC-Agg.Review.
+
+Run the following commands from within the root directory to run the services in your local machione once you pulle down the repository(alternatively you can follow the steps below):
+
+```bash
+fork 'https://github.com/huang-pei-mei/FEC-Agg.Review'
+git clone /path/to/clone.git
+cd FEC-Agg.Review
+npm install
+npm run server
+ -->
 ```
+
 
 
--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  ProgressBar from './ProgressBar.jsx';
+import ProgressBar from './ProgressBar.jsx';
 import StarRatings from 'react-star-ratings';
 
 
@@ -14,7 +14,7 @@ function getStars(average) {
   } else if (remaining < 0.8) {
     return minStars + 0.5;
   } else {
-    return minStars + 1 ;
+    return minStars + 1;
   }
 
 }
@@ -33,153 +33,152 @@ const ReviewList = (props) => {
     '5': '\u2605\u2605\u2605\u2605\u2605'
   }
 
-
   return (
 
 
-<div>
-<div className="main-heading">
-What listeners say about this book
+    <div>
+      <div className="main-heading">
+        What listeners say about {props.currentBook.title}
+      </div>
+
+      <div className="rating-heading">
+        Average Customer Ratings
     </div>
 
-    <div className="rating-heading">
-      Average Customer Ratings
+      <div className="threeTables">
+        <div className="rating">Overall</div>
+        <div className="overallRating">Overall Ratings</div>
+
+        <span>
+          <ul className="table1">
+            <li className="averagereview">
+              <StarRatings rating={getStars(props.review.overall.average)} starRatedColor='#FFA000' starDimension='24px' starSpacing='3px' starEmptyColor='rgb(230, 230, 230)' />
+              <span>&nbsp;&nbsp;{props.review.overall.average} Out of 5</span>
+            </li>
+
+            <li id="overall">
+              <span>5 Stars&nbsp;&nbsp;</span>
+              {<progress id="bar" value="80" max="100"> 80% </progress>}
+              <span>&nbsp;&nbsp;{props.review.overall.fiveStars}</span>
+            </li>
+
+            <li id="overall">
+              <span>4 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="30" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.overall.fourStars}</span>
+            </li>
+
+            <li id="overall">
+              <span>3 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="20" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.overall.threeStars}</span>
+            </li>
+
+            <li id="overall">
+              <span>2 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="10" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.overall.twoStars}</span>
+            </li>
+
+            <li id="overall">
+              <span>1 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="5" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.overall.oneStars}</span>
+            </li>
+
+          </ul>
+        </span>
+
+
+        <div className="performancerating">Performance</div>
+
+
+        <span>
+          <ul className="table2">
+            <li className="averagereview">
+              <StarRatings rating={getStars(props.review.performance.average)} starRatedColor='#FFA000' starDimension='24px' starSpacing='3px' starEmptyColor='rgb(230, 230, 230)' />
+              <span>&nbsp;&nbsp;{props.review.performance.average} Out of 5</span>
+            </li>
+
+            <li id="performance">
+              <span>5 Stars&nbsp;&nbsp;</span>
+              {<progress id="bar" value="80" max="100"> 80% </progress>}
+              <span>&nbsp;&nbsp;{props.review.performance.fiveStars}</span>
+            </li>
+
+            <li id="performance">
+              <span>4 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="30" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.performance.fourStars}</span>
+            </li>
+
+            <li id="performance">
+              <span>3 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="20" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.performance.threeStars}</span>
+            </li>
+
+            <li id="performance">
+              <span>2 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="10" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.performance.twoStars}</span>
+            </li>
+
+            <li id="performance">
+              <span>1 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="5" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.performance.oneStars}</span>
+            </li>
+
+          </ul>
+        </span>
+
+        <div className="storyrating">Story</div>
+
+
+        <span>
+          <ul className="table3">
+            <li className="averagereview">
+              <StarRatings rating={getStars(props.review.story.average)} starRatedColor='#FFA000' starDimension='24px' starSpacing='3px' starEmptyColor='rgb(230, 230, 230)' starBorders='#FFA000' />
+              <span>&nbsp;&nbsp;{props.review.story.average} Out of 5</span>
+            </li>
+
+            <li id="story">
+              <span>5 Stars&nbsp;&nbsp;</span>
+              {<progress id="bar" value="80" max="100"> 80% </progress>}
+              <span>&nbsp;&nbsp;{props.review.story.fiveStars}</span>
+            </li>
+
+            <li id="story">
+              <span>4 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="30" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.story.fourStars}</span>
+            </li>
+
+            <li id="story">
+              <span>3 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="20" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.story.threeStars}</span>
+            </li>
+
+            <li id="story">
+              <span>2 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="10" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.story.twoStars}</span>
+            </li>
+
+            <li id="story">
+              <span>1 Stars&nbsp;&nbsp;</span>
+              <progress id="bar" value="5" max="100"> 80% </progress>
+              <span>&nbsp;&nbsp;{props.review.story.oneStars}</span>
+            </li>
+
+          </ul>
+        </span>
+      </div>
     </div>
 
-<div className="threeTables">
-   <div className="rating">Overall</div>
-  <div className="overallRating">Overall Ratings</div>
-
-<span>
- 	<ul className="table1">
-<li className="averagereview">
-<StarRatings rating={getStars(props.review.overall.average)} starRatedColor='#FFA000' starDimension='24px' starSpacing='3px' starEmptyColor='rgb(230, 230, 230)'/>
-<span>&nbsp;&nbsp;{props.review.overall.average} Out of 5</span>
-</li>
-
-<li id="overall">
-<span>5 Stars&nbsp;&nbsp;</span>
-{<progress id="bar" value="80" max="100"> 80% </progress>}
-<span>&nbsp;&nbsp;{props.review.overall.fiveStars}</span>
-</li>
-
-<li id="overall">
-<span>4 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="30" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.overall.fourStars}</span>
-</li>
-
-<li id="overall">
-<span>3 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="20" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.overall.threeStars}</span>
-</li>
-
-<li id="overall">
-<span>2 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="10" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.overall.twoStars}</span>
-</li>
-
-<li id="overall">
-<span>1 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="5" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.overall.oneStars}</span>
-</li>
-
-</ul>
-</span>
-
-
-<div className="performancerating">Performance</div>
-
-
-<span>
- 	<ul className="table2">
-<li className="averagereview">
-<StarRatings rating={getStars(props.review.performance.average)} starRatedColor='#FFA000' starDimension='24px' starSpacing='3px' starEmptyColor='rgb(230, 230, 230)'/>
-<span>&nbsp;&nbsp;{props.review.performance.average} Out of 5</span>
-</li>
-
-<li id="performance">
-<span>5 Stars&nbsp;&nbsp;</span>
-{<progress id="bar" value="80" max="100"> 80% </progress>}
-<span>&nbsp;&nbsp;{props.review.performance.fiveStars}</span>
-</li>
-
-<li id="performance">
-<span>4 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="30" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.performance.fourStars}</span>
-</li>
-
-<li id="performance">
-<span>3 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="20" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.performance.threeStars}</span>
-</li>
-
-<li id="performance">
-<span>2 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="10" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.performance.twoStars}</span>
-</li>
-
-<li id="performance">
-<span>1 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="5" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.performance.oneStars}</span>
-</li>
-
-</ul>
-</span>
-
-<div className="storyrating">Story</div>
-
-
-<span>
- 	<ul className="table3">
-<li className="averagereview">
-<StarRatings rating={getStars(props.review.story.average)} starRatedColor='#FFA000' starDimension='24px' starSpacing='3px' starEmptyColor='rgb(230, 230, 230)' starBorders='#FFA000'/>
-<span>&nbsp;&nbsp;{props.review.story.average} Out of 5</span>
-</li>
-
-<li id="story">
-<span>5 Stars&nbsp;&nbsp;</span>
-{<progress id="bar" value="80" max="100"> 80% </progress>}
-<span>&nbsp;&nbsp;{props.review.story.fiveStars}</span>
-</li>
-
-<li id="story">
-<span>4 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="30" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.story.fourStars}</span>
-</li>
-
-<li id="story">
-<span>3 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="20" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.story.threeStars}</span>
-</li>
-
-<li id="story">
-<span>2 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="10" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.story.twoStars}</span>
-</li>
-
-<li id="story">
-<span>1 Stars&nbsp;&nbsp;</span>
-<progress id="bar" value="5" max="100"> 80% </progress>
-<span>&nbsp;&nbsp;{props.review.story.oneStars}</span>
-</li>
-
-</ul>
-</span>
-</div>
-</div>
-
-)
+  )
 
 };
 
